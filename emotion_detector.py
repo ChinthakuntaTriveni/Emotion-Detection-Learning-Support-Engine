e3379fc02f91f46636777e6d2a5abb7d8da93cbd
ChinthakuntaTriveni <chinthakuntatriveni773@gmail.com>
2026-07-15 08:31:18 +0530

    Final Project Submission

---
+++ emotion_detector.py
@@ -0,0 +1,64 @@
+# Emotion keywords
+
+emotion_keywords = {
+
+    "Confident": [
+        "confident", "easy", "understand",
+        "completed", "good", "excellent",
+        "learned", "success"
+    ],
+
+    "Confused": [
+        "confused", "don't understand",
+        "unclear", "difficult",
+        "question", "help", "why"
+    ],
+
+    "Curious": [
+        "curious", "interesting",
+        "want to know", "explore",
+        "learn more", "how"
+    ],
+
+    "Frustrated": [
+        "frustrated", "angry",
+        "failed", "stress",
+        "problem", "worried",
+        "hard"
+    ],
+
+    "Bored": [
+        "bored",
+        "boring",
+        "sleepy",
+        "tired",
+        "not interested"
+    ]
+}
+
+
+def detect_emotion(text):
+
+    text = text.lower()
+
+    scores = {}
+
+    for emotion, words in emotion_keywords.items():
+
+        score = 0
+
+        for word in words:
+
+            if word in text:
+
+                score += 1
+
+        scores[emotion] = score
+
+    best = max(scores, key=scores.get)
+
+    if scores[best] == 0:
+
+        return "Neutral"
+
+    return best
